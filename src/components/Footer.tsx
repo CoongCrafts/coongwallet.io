29145db6741c4804a0a85ab04f39d37398d87bd9
Thang X. Vu <thang@coongcrafts.io>
2023-03-19 15:19:55 +0700

    refactor: adjust social icon size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,13 +9,13 @@ const Footer: FC<Props> = ({ className = '' }) => {
     <footer className={`${className} border-t border-black/10`}>
       <Container>
         <div className='flex flex-col sm:flex-row justify-between items-center py-4 gap-4'>
-          <span>Copyright &copy; 2023 - Coong Wallet</span>
-          <div className='flex gap-4'>
+          <span>Copyright &copy; 2023 Coong Wallet</span>
+          <div className='flex gap-6'>
             <a href='https://twitter.com/CoongWallet' target='_blank'>
-              <FontAwesomeIcon icon={faTwitter} size='xl' className='text-gray-600 hover:text-gray-800' />
+              <FontAwesomeIcon icon={faTwitter} size='2x' className='text-gray-600 hover:text-gray-800' />
             </a>
-            <a href='https://github.com/CoongCrafts' target='_blank'>
-              <FontAwesomeIcon icon={faGithub} size='xl' className='text-gray-600 hover:text-gray-800' />
+            <a href='https://github.com/CoongCrafts/coong-wallet' target='_blank'>
+              <FontAwesomeIcon icon={faGithub} size='2x' className='text-gray-600 hover:text-gray-800' />
             </a>
           </div>
         </div>
